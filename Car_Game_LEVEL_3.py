0b82779f26eded5cfb958e87783b36149e3f3446
Anson-Anthony <ansonanthony@riccarton.school.nz>
2025-05-07 09:52:31 +1200

    Add files via upload

--- Car_Game_LEVEL_3.py
+++ Car_Game_LEVEL_3.py
@@ -20,15 +20,37 @@ RED = (0, 255, 0)
 ROAD_WIDTH = 600
 LANE_MARKER_WIDTH = 10
 LANE_MARKER_HEIGHT = 50
+score = 0
+high_score = 0
+
+speed = 5
+
+try:
+    with open("highscore.txt") as file:
+        high_score = int(file.read())
+except:
+    high_score = 0
 
 
+road_markers = []
+for lane_x in [100, 250, 400]:
+    for i in range(5):
+        road_markers.append([lane_x - LANE_MARKER_WIDTH//2, i * 200])
+clock = pygame.time.Clock()
 
 def draw_road():
     
     pygame.draw.rect(screen, GRAY, 
                     (SCREEN_WIDTH//2 - ROAD_WIDTH//2, 0, ROAD_WIDTH, SCREEN_HEIGHT))
     
-
+   
+    for marker in road_markers:
+        pygame.draw.rect(screen, WHITE, 
+                        (marker[0], marker[1], LANE_MARKER_WIDTH, LANE_MARKER_HEIGHT))
+        
+        marker[1] += speed
+        if marker[1] > SCREEN_HEIGHT:
+            marker[1] = -LANE_MARKER_HEIGHT
 
 
 running = True
@@ -37,8 +59,15 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    font = pygame.font.Font(None, 36)
+    text = font.render(f"Score: {score}", True, WHITE)
+    screen.blit(text, (10, 10))
+    text = font.render(f"High_score: {high_score}", True, WHITE)
+    screen.blit(text, (10, 50))
+
     screen.fill(BLACK)
     draw_road()
     pygame.display.update()
+    clock.tick(60)
 
 pygame.quit()
