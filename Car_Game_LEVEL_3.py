25e5a9a578662ddd7f92febc75ba0655fe929d3f
Anson-Anthony <ansonanthony@riccarton.school.nz>
2025-05-09 17:52:27 +1200

    Add files via upload

--- Car_Game_LEVEL_3.py
+++ Car_Game_LEVEL_3.py
@@ -7,7 +7,7 @@ SCREEN_WIDTH = 500
 SCREEN_HEIGHT = 720
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Car_Game")
-game_icon=pygame.image.load('game_icon.png')
+game_icon = pygame.image.load('game_icon.png')
 pygame.display.set_icon(game_icon)
 
 BLACK = (0, 0, 0)
@@ -17,59 +17,63 @@ GREEN = (188, 227, 199)
 BLUE = (32, 32, 200)
 RED = (0, 255, 0)
 
-ROAD_WIDTH = 600
+# lane varable
+ROAD_WIDTH = 400
 LANE_MARKER_WIDTH = 10
 LANE_MARKER_HEIGHT = 50
 
-#where player spawns
-washing_machine_X = 250
+# player varable
+washing_machine_X = 200
 washing_machine_Y = 600
-#player score
 score = 0
-#player high score
 high_score = 0
-#speed of road, how fast it goes down, and how fast enemie car comes
 speed = 5
 game_over = False
 
+# import images
 washing_machine = pygame.image.load("Player_washing_machine.png")
 washing_machine = pygame.transform.scale(washing_machine, (100, 100))
+Normal_car = pygame.image.load("Lemon_car.png")
+Normal_car = pygame.transform.scale(Normal_car, (100, 100))
 
-
+# high score save file
 try:
     with open("highscore.txt") as file:
         high_score = int(file.read())
 except:
     high_score = 0
 
-
+# Create 4 lane
 road_markers = []
-for lane_x in [100, 250, 400]:
+for lane_x in [100, 200, 300, 400]: 
     for i in range(5):
         road_markers.append([lane_x - LANE_MARKER_WIDTH//2, i * 200])
+
+# Enemy cars
+enemies = []
+for _ in range(4):
+    Normal_car_x = random.choice([100, 200, 300, 400]) 
+    Normal_car_y = random.randint(-1500, -100)
+    enemies.append([Normal_car_x, Normal_car_y, random.randint(3, 6)])
+
+#clock tick varable
 clock = pygame.time.Clock()
 
 def draw_road():
-    
     pygame.draw.rect(screen, GRAY, 
                     (SCREEN_WIDTH//2 - ROAD_WIDTH//2, 0, ROAD_WIDTH, SCREEN_HEIGHT))
     
-   
     for marker in road_markers:
         pygame.draw.rect(screen, WHITE, 
                         (marker[0], marker[1], LANE_MARKER_WIDTH, LANE_MARKER_HEIGHT))
-        
         marker[1] += speed
         if marker[1] > SCREEN_HEIGHT:
             marker[1] = -LANE_MARKER_HEIGHT
 
-
-
-
     screen.blit(washing_machine, (washing_machine_X, washing_machine_Y))
-
-
-
+    
+    for enemy in enemies:
+        screen.blit(Normal_car, (enemy[0], enemy[1]))
 
 running = True
 while running:
@@ -77,14 +81,18 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        
+
+    screen.fill(BLACK)
+    draw_road()
+    
+    
     font = pygame.font.Font(None, 36)
     text = font.render(f"Score: {score}", True, WHITE)
     screen.blit(text, (10, 10))
     text = font.render(f"High_score: {high_score}", True, WHITE)
     screen.blit(text, (10, 50))
-
-    screen.fill(BLACK)
-    draw_road()
+    
     pygame.display.update()
     clock.tick(60)
 
